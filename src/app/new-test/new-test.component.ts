67ff1a3f214ea1e0434b86545fb9d5f944015efe
abouttocrash <luisjaime8@hotmail.com>
2024-11-14 10:50:34 -0600

    minor changes

--- src/app/new-test/new-test.component.ts
+++ src/app/new-test/new-test.component.ts
@@ -1,18 +1,57 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
-
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { RichTextWaveAutoCompleteComponent } from '../wave-autocomplete/rich-text-wave-auto-complete/rich-text-wave-auto-complete.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject, merge, tap } from 'rxjs';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 @Component({
   selector: 'app-new-test',
   standalone: true,
-  imports: [FormsModule,MatInput,MatLabel, ReactiveFormsModule,MatFormField,RouterLink],
+  imports: [MatFormFieldModule,CommonModule, MatProgressSpinnerModule,FormsModule,MatInput,MatLabel, ReactiveFormsModule,MatFormField,RouterLink,RichTextWaveAutoCompleteComponent],
   templateUrl: './new-test.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './new-test.component.scss'
 })
 export class NewTestComponent {
   testName:string = ""
+  reqName:string = ""
+  submitted = false;
+  loading$ = new BehaviorSubject<boolean>(false);
   namePlaceholder = "Enter a name for the test case"
   reqPlaceholder = "Match the test case to its parent requirement" 
+  signUpForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    req: new FormControl('', [Validators.required])
+  });
+
+  async onSubmit() {
+    if (this.signUpForm.invalid) return;
+    this.loading$.next(true)
+    await this.timeout(1000)
+    this.loading$.next(false)
+    this.submitted = true
+
+  }
+  constructor(){
+   
+  }
+
+  timeout(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+  applyFilter(event: Event) {
+    
+    const filterValue = (event.target as HTMLInputElement).value;
+    
+  }
+
+  createTest(){
+   
+    
+  }
 }
